63d6c57acc892ee49295db9af33b6690af2307f3
ahmed <ahmed.abdullahi@eoncollective.com>
2024-07-26 15:37:02 +0300

    remove dbt models

--- athena/models/fct_stg_CUSTOMERS.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    stg_CUSTOMERS.ID AS "ID",
-    stg_CUSTOMERS.FIRST_NAME AS "FIRST_NAME",
-    stg_CUSTOMERS.LAST_NAME AS "LAST_NAME",
-    stg_CUSTOMERS.__load_date AS "__load_date",
-    current_date() AS "__load_date_1"
-FROM {{ref('My Location', 'stg_CUSTOMERS')}} "stg_CUSTOMERS"
--- athena/models/stg_ORDERS.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    ORDERS.ID AS "ID",
-    ORDERS.USER_ID AS "USER_ID",
-    ORDERS.ORDER_DATE AS "ORDER_DATE",
-    ORDERS.STATUS AS "STATUS",
-    current_date() AS "__load_date"
-FROM {{ref('My Location', 'ORDERS')}} "ORDERS"
